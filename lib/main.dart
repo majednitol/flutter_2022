62202e561de01ae43c9a63e95fb950c2baad7b5a
majed <majadrahman7@gmail.com>
2022-03-13 03:33:33 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,148 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "flutter",
-    home: const SnackBarPage(),
+    home: const HomePage(),
     theme: ThemeData(primarySwatch: Colors.blue),
     debugShowCheckedModeBanner: false,
   ));
 }
 
-class SnackBarPage extends StatelessWidget {
-  const SnackBarPage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('SnackBar'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              final snackBar = SnackBar(
-                content: const Text('Yay! A SnackBar!'),
-                action: SnackBarAction(
-                  label: 'Undo',
-                  onPressed: () {},
-                ),
-              );
-
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            },
-            child: const Text('Show SnackBar'),
+      appBar: AppBar(
+        title: const Text('ListTile'),
+      ),
+      body: ListView(children: const [
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
           ),
-        ));
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+        ListTile(
+          title: Text("Fluttter App Development"),
+          subtitle: Text("Learn more earn more"),
+          leading: CircleAvatar(
+            child: Icon(Icons.account_circle_rounded),
+          ),
+          trailing: Icon(Icons.camera),
+        ),
+      ]),
+    );
   }
 }
