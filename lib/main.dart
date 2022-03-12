578c98bdc8bbec6a95799d14d33d54c92c3430a9
majed <majadrahman7@gmail.com>
2022-03-12 22:40:01 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:liquid_swipe/liquid_swipe.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "awsome app",
-    home: HomePage(),
+    home: const HomePage(),
     theme: ThemeData(
-      primarySwatch: Colors.purple,
+      primarySwatch: Colors.deepPurple,
     ),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
 class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
-  final pages = [
-    Container(
-      color: Colors.red,
-    ),
-    Container(
-      color: Colors.green,
-    ),
-    Container(
-      color: Colors.blue,
-    ),
-    Container(
-      color: Colors.yellow,
-    )
-  ];
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('liquidSwipe'),
+          title: const Text('Card and gridview'),
         ),
-        body: LiquidSwipe(pages: pages));
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            children: [
+              Container(width: 100, height: 100, color: Colors.red),
+              Container(width: 100, height: 100, color: Colors.green),
+              Container(width: 100, height: 100, color: Colors.black),
+              Container(width: 100, height: 100, color: Colors.red),
+              Container(width: 100, height: 100, color: Colors.orange),
+              Container(width: 100, height: 100, color: Colors.blue),
+            ],
+          ),
+        )
+
+        // Center(
+        //   child: Card(
+        //     child: Container(
+        //       height: 200,
+        //       width: 200,
+        //     ),
+        //     elevation: 10,
+        //     shape: const RoundedRectangleBorder(
+        //         borderRadius:
+        //             BorderRadius.only(bottomLeft: Radius.circular(20))),
+        //   ),
+        // )
+        );
   }
 }
