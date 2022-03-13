fdaded34d66267a303e78b98f813836bd2919fa3
majed <majadrahman7@gmail.com>
2022-03-13 20:46:30 +0600

    done

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "flutter",
+    home: const HomePage(),
+    theme: ThemeData(primarySwatch: Colors.amber),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool selected = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              selected ? "checked" : "unchecked",
+              style: const TextStyle(fontSize: 30),
+            ),
+            Checkbox(
+                value: selected,
+                onChanged: (value) {
+                  setState(() {
+                    selected = value!;
+                  });
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
