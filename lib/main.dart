111fb6f6c4e13b43b9b5b8a9051a46066d8796ec
majed <majadrahman7@gmail.com>
2022-03-14 00:24:38 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+
+// problem this topic
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,3 +11,23 @@ void main() {
   ));
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [Container(width: 300, height: 300, color: Colors.red),
+        BackdropFilter(filter: ImageFilter.blur(sigmaX))
+        ],
+      ),
+    );
+  }
+}
