8347722a03bc20f454acbea85b31f286e11be1c5
majed <majadrahman7@gmail.com>
2022-03-13 15:59:03 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -18,25 +17,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var currentvalue = 10.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Toast '),
         ),
-        body: Center(
-          child: ElevatedButton(
-              onPressed: (() {
-                Fluttertoast.showToast(
-                    msg: "This is Center Short Toast",
-                    toastLength: Toast.LENGTH_SHORT,
-                    gravity: ToastGravity.BOTTOM,
-                    timeInSecForIosWeb: 1,
-                    backgroundColor: Colors.red,
-                    textColor: Colors.white,
-                    fontSize: 16.0);
-              }),
-              child: const Text("click")),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "learn flutter",
+              style: TextStyle(fontSize: currentvalue),
+            ),
+            Slider(
+                activeColor: Colors.amber,
+                inactiveColor: Colors.red,
+                divisions: 5,
+                label: currentvalue.round().toString(),
+                min: 10,
+                max: 50,
+                value: currentvalue,
+                onChanged: (value) {
+                  setState(() {
+                    currentvalue = value;
+                    print(currentvalue);
+                  });
+                })
+          ],
         ));
   }
 }
