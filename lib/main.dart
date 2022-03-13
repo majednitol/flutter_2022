0c1ea1f3d11b43709af0c736bf16b1657d319f40
majed <majadrahman7@gmail.com>
2022-03-14 00:39:57 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Title'),
+          title: Text('Transform'),
         ),
         body: SingleChildScrollView(
           child: Column(
