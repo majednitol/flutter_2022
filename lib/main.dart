fffd6e203d027999746faec5f4d431b6a7152841
majed <majadrahman7@gmail.com>
2022-03-14 00:38:21 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
   runApp(MaterialApp(
     title: "flutter",
     home: const HomePage(),
-    theme: ThemeData(primarySwatch: Colors.red),
+    theme: ThemeData(primarySwatch: Colors.green),
   ));
 }
 
@@ -22,27 +22,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-            child: Transform.rotate(
-                angle: pi / 4,
-                child: Container(height: 100, width: 100, color: Colors.red))),
-        SizedBox(
-          height: 100,
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Title'),
         ),
-        Center(
-            child: Transform.scale(
-                scale: 2.5,
-                child: Container(height: 100, width: 100, color: Colors.red))),
-        SizedBox(
-          height: 100,
-        ),
-        Center(
-            child: Transform.translate(
-                offset: Offset(20, 60),
-                child: Container(height: 100, width: 100, color: Colors.red))),
-      ],
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                  child: Transform.rotate(
+                      angle: pi / 4,
+                      child: Container(
+                          height: 100, width: 100, color: Colors.red))),
+              SizedBox(
+                height: 100,
+              ),
+              Center(
+                  child: Transform.scale(
+                      scale: 1.5,
+                      child: Container(
+                          height: 100, width: 100, color: Colors.blue))),
+              SizedBox(
+                height: 100,
+              ),
+              Center(
+                  child: Transform.translate(
+                      offset: Offset(20, 60),
+                      child: Container(
+                          height: 100, width: 100, color: Colors.purple))),
+            ],
+          ),
+        ));
   }
 }
