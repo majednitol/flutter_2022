71e16ad53392e2cbce756c73e08b0e29cb6938ae
majed <majadrahman7@gmail.com>
2022-03-15 00:35:02 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_2022/page1.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "flutter",
     home: const HomePage(),
     theme: ThemeData(primarySwatch: Colors.green),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), (() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Login()),
+      );
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +37,15 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.yellow,
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset("assets/a.png"),
+            Image.asset("assets/a.png", height: 300, width: 300),
+            SizedBox(
+              height: 100,
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(Colors.white),
+            )
           ],
         ),
       ),
--- lib/page1.dart
+++ lib/page1.dart
@@ -1,22 +1,107 @@
 import 'package:flutter/material.dart';
 
-class Page1 extends StatefulWidget {
-  const Page1({Key? key}) : super(key: key);
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
 
-  @override
-  _Page1State createState() => _Page1State();
-}
-
-class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Title'),
-      ),
-      body: Container(
-        child: Center(child: Text('page1')),
-      ),
+      resizeToAvoidBottomInset: false,
+      body: Stack(fit: StackFit.expand, children: [
+        Image.asset(
+          "assets/m.jpg",
+          fit: BoxFit.cover,
+          color: Colors.black54,
+          colorBlendMode: BlendMode.darken,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/k.png", width: 120.0, height: 120.0),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Stack(
+                children: [
+                  SingleChildScrollView(
+                    child: Container(
+                        height: 350.0,
+                        width: 390.0,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 25.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: TextField(
+                                autofocus: false,
+                                autocorrect: false,
+                                style: TextStyle(fontSize: 20.0),
+                                decoration: InputDecoration(
+                                    hintText: "Username",
+                                    border: InputBorder.none,
+                                    filled: true,
+                                    fillColor: Colors.grey[200],
+                                    contentPadding: const EdgeInsets.all(15.0)),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 10.0,
+                                  right: 20.0,
+                                  left: 20.0,
+                                  bottom: 10.0),
+                              child: TextField(
+                                autofocus: false,
+                                obscureText: true,
+                                autocorrect: false,
+                                style: TextStyle(fontSize: 20.0),
+                                decoration: InputDecoration(
+                                    hintText: "Password",
+                                    border: InputBorder.none,
+                                    filled: true,
+                                    fillColor: Colors.grey[200],
+                                    contentPadding: const EdgeInsets.all(15.0)),
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 17.0),
+                                  child: Text(
+                                    "Forget password ?",
+                                    style: TextStyle(
+                                        fontSize: 14.0,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 40.0),
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.red,
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 16, horizontal: 100),
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(fontSize: 20.0),
+                                ))
+                          ],
+                        )),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ]),
     );
   }
 }
