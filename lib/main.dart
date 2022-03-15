dee6183b45785e81e6a251de8da3a11477dae301
majed <majadrahman7@gmail.com>
2022-03-15 17:12:16 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,16 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_2022/page1.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "flutter",
-    home: const HomePage(),
+    home: AnimatedSplashScreen(
+      splash: "assets/a.png",
+      nextScreen: Login(),
+      splashTransition: SplashTransition.decoratedBoxTransition,
+    ),
     theme: ThemeData(primarySwatch: Colors.green),
     debugShowCheckedModeBanner: false,
   ));
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 5), (() {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => Login()),
-      );
-    }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.yellow,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset("assets/a.png", height: 300, width: 300),
-            SizedBox(
-              height: 100,
-            ),
-            CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation(Colors.white),
-            )
-          ],
-        ),
-      ),
-    );
-  }
 }
