cfd6748aca883b51138ee59c1491820d043cbafc
majed <majadrahman7@gmail.com>
2022-03-15 17:41:51 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Stack'),
+          title: const Text('Stack and positioned'),
         ),
         body: Center(
           child: Stack(children: [
             Container(width: 300, height: 300, color: Colors.amber),
             Container(width: 300, height: 200, color: Colors.red),
             Container(width: 300, height: 100, color: Colors.green),
+            Positioned(
+                child: Container(
+                    width: 100,
+                    height: 50,
+                    color: Colors.purple,
+                    child: const Center(child: Text("positioned"))),
+                // top: 50,
+                left: 50,
+                bottom: 50)
           ]),
         ));
   }
