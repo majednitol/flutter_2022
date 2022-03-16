e4bb82e8e6d604cc9343ed82cbaabac399422564
Your Name <youremail@yourdomain.com>
2022-03-16 21:48:10 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,68 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//     title: "Flutter",
+//     home: HomePage(),
+//     theme: ThemeData(primarySwatch: Colors.blue),
+//   ));
+// }
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   int value = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('setState and Provider'),
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Container(
+//                   child: Text(
+//                 value.toString(),
+//                 style: TextStyle(fontSize: 30),
+//               )),
+//               SizedBox(
+//                 height: 40,
+//               ),
+//               ElevatedButton(
+//                   style: ElevatedButton.styleFrom(
+//                     primary: Colors.red,
+//                   ),
+//                   onPressed: () {
+//                     setState(() {
+//                       value++;
+//                     });
+//                   },
+//                   child: Text("click here"))
+//             ],
+//           ),
+//         ));
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 void main() {
-  runApp(MaterialApp(
-    title: "Flutter",
-    home: HomePage(),
-    theme: ThemeData(primarySwatch: Colors.blue),
+  runApp(ChangeNotifierProvider(
+    create: (context) => Data(),
+    child: MaterialApp(
+      title: "Flutter",
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    ),
   ));
 }
 
@@ -18,16 +76,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final providerdata = Provider.of<Data>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Title'),
-      ),
-      body: Center(
-        child: Container(
-            color: Colors.green,
-            height: MediaQuery.of(context).size.height / 3,
-            width: MediaQuery.of(context).size.width / 2),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('setState and Provider'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  child: Text(
+                providerdata.value.toString(),
+                style: TextStyle(fontSize: 30),
+              )),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                  ),
+                  onPressed: () {
+                    providerdata.increment();
+                  },
+                  child: Text("click here"))
+            ],
+          ),
+        ));
+  }
+}
+
+class Data extends ChangeNotifier {
+  int value = 0;
+  increment() {
+    value++;
+    notifyListeners();
   }
 }
