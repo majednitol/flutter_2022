75e39f8db2ad8c29621af7cb86dd91b7241df9c4
Your Name <youremail@yourdomain.com>
2022-03-19 04:34:05 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,149 @@
+// validated a sign up form
 import 'package:flutter/material.dart';
-import 'package:flutter_2022/page1.dart';
-import 'package:flutter_2022/page2.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-    theme: ThemeData(primarySwatch: Colors.deepOrange),
-    initialRoute: "/",
-    routes: {
-      "/first": (context) => Page1(),
-      "/seond": (context) => Page2(),
-    },
-  ));
+  runApp(const MyApp());
 }
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Define a key to access the form
+  final _formKey = GlobalKey<FormState>();
+
+  String _userEmail = '';
+  String _userName = '';
+  String _password = '';
+  String _confirmPassword = '';
+
+  // This function is triggered when the user press the "Sign Up" button
+  void _trySubmitForm() {
+    final bool? isValid = _formKey.currentState?.validate();
+    if (isValid == true) {
+      debugPrint('Everything looks good!');
+      debugPrint(_userEmail);
+      debugPrint(_userName);
+      debugPrint(_password);
+      debugPrint(_confirmPassword);
+
+      
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("InitialRoute,Route,PushNamed,Push,pop"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: (() {
-            
-            Navigator.pushNamed(
-              context,
-              "/first",
-            );
-          }),
-          child: Text('Homepage'),
+      body: Container(
+        color: Colors.deepOrange,
+        alignment: Alignment.center,
+        child: Center(
+          child: Card(
+            margin: const EdgeInsets.symmetric(horizontal: 35),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      /// Eamil
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: 'Email'),
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'Please enter your email address';
+                          }
+                          // Check if the entered email has the right format
+                          if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                            return 'Please enter a valid email address';
+                          }
+                          // Return null if the entered email is valid
+                          return null;
+                        },
+                        onChanged: (value) => _userEmail = value,
+                      ),
+
+                      /// username
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Username'),
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'This field is required';
+                          }
+                          if (value.trim().length < 4) {
+                            return 'Username must be at least 4 characters in length';
+                          }
+                          // Return null if the entered username is valid
+                          return null;
+                        },
+                        onChanged: (value) => _userName = value,
+                      ),
+
+                      /// Password
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Password'),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'This field is required';
+                          }
+                          if (value.trim().length < 8) {
+                            return 'Password must be at least 8 characters in length';
+                          }
+                          // Return null if the entered password is valid
+                          return null;
+                        },
+                        onChanged: (value) => _password = value,
+                      ),
+
+                      /// Confirm Password
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is required';
+                          }
+
+                          if (value != _password) {
+                            return 'Confimation password does not match the entered password';
+                          }
+
+                          return null;
+                        },
+                        onChanged: (value) => _confirmPassword = value,
+                      ),
+                      const SizedBox(height: 20),
+                      Container(
+                          alignment: Alignment.centerRight,
+                          child: OutlinedButton(
+                              onPressed: _trySubmitForm,
+                              child: const Text('Sign Up')))
+                    ],
+                  )),
+            ),
+          ),
         ),
       ),
     );
