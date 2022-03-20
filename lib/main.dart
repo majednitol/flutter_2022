3c107626cc4bdb4f413adefefa285df99aa13d2e
Your Name <youremail@yourdomain.com>
2022-03-20 23:30:55 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'dart:async';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Kindacode.com',
+  runApp(MaterialApp(
     home: HomePage(),
   ));
 }
@@ -20,38 +14,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  StreamSubscription? subscription;
-  Future checkConnection() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult == ConnectivityResult.mobile) {
-      Fluttertoast.showToast(msg: "connected with mobile");
-    } else if (connectivityResult == ConnectivityResult.mobile) {
-      Fluttertoast.showToast(msg: "connected with wifi");
-    } else {
-      Fluttertoast.showToast(msg: "Not connected ");
-    }
-  }
-// realtime connection checking
-  @override
-  void initState() {
-    super.initState();
-    subscription = Connectivity().onConnectivityChanged.listen((event) {
-      checkConnection();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Connectivity'),
-        ),
-        body: Center(
-            child: Container(
-          child: ElevatedButton(
-            onPressed: checkConnection,
-            child: Text('check Connectivity'),
-          ),
-        )));
+      appBar: AppBar(
+        title: const Text('Responsive Ui design'),
+      ),
+      body: Container(),
+    );
   }
 }
