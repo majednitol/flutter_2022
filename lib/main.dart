da666adbf0cd5a0d50cd16a0b7e4efc70b5f8e48
Your Name <youremail@yourdomain.com>
2022-03-20 23:17:44 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-// validated a sign up form
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -17,15 +20,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  StreamSubscription? subscription;
+  Future checkConnection() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile) {
+      Fluttertoast.showToast(msg: "connected with mobile");
+    } else if (connectivityResult == ConnectivityResult.mobile) {
+      Fluttertoast.showToast(msg: "connected with wifi");
+    } else {
+      Fluttertoast.showToast(msg: "Not connected ");
+    }
+  }
+// realtime connection checking
+  @override
+  void initState() {
+    super.initState();
+    subscription = Connectivity().onConnectivityChanged.listen((event) {
+      checkConnection();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text(''),
+          title: const Text('Flutter Connectivity'),
         ),
         body: Center(
             child: Container(
-          child: Text(""),
+          child: ElevatedButton(
+            onPressed: checkConnection,
+            child: Text('check Connectivity'),
+          ),
         )));
   }
 }
