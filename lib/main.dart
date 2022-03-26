60b7952c94f2ce3297c0393b8fd50f74cb064daa
Your Name <youremail@yourdomain.com>
2022-03-26 11:56:47 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,56 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
-import 'Models/posts_Model.dart';
+
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
   ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PostsModel> postList = [];
+  Future<List<PostsModel>> getApi() async {
+    final response = await http.get(Uri.parse(""));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getApi(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          } else {
+            return ListView.builder(
+                itemCount: postList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Column(
+                      children: [
+                        Text("title",style: TextStyle(fontSize: 15),),SizedBox(height: 3,)
+                      ],
+                    ),
+                  );
+                });
+          }
+        });
+  }
 }
