30c795997d1389baf5bb55dc996252e924d7ce44
Your Name <youremail@yourdomain.com>
2022-03-27 20:07:38 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -5,34 +5,25 @@ import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
-    home: ExampleTwo(),
+    home: ExampleFour(),
   ));
 }
 
-class ExampleTwo extends StatefulWidget {
-  const ExampleTwo({Key? key}) : super(key: key);
+class ExampleFour extends StatefulWidget {
+  const ExampleFour({Key? key}) : super(key: key);
 
   @override
-  _ExampleTwoState createState() => _ExampleTwoState();
+  _ExampleFourState createState() => _ExampleFourState();
 }
 
-class _ExampleTwoState extends State<ExampleTwo> {
-  List<Photos> photosList = [];
+class _ExampleFourState extends State<ExampleFour> {
+  var data;
+  Future<void> getUserApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
 
-  Future<List<Photos>> getPhotos() async {
-    final response = await http
-        .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
-    var data = jsonDecode(response.body.toString());
-
-    print(data);
     if (response.statusCode == 200) {
-      for (Map i in data) {
-        Photos photos = Photos(title: i['title'], url: i['url'], id: i['id']);
-        photosList.add(photos);
-      }
-      return photosList;
-    } else {
-      return photosList;
+      data = jsonDecode(response.body.toString());
     }
   }
 
@@ -41,38 +32,76 @@ class _ExampleTwoState extends State<ExampleTwo> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('array json '),
+        title: Text('Api Course'),
       ),
       body: Column(
         children: [
           Expanded(
             child: FutureBuilder(
-                future: getPhotos(),
-                builder: (context, AsyncSnapshot<List<Photos>> snapshot) {
+              future: getUserApi(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
                   return ListView.builder(
-                      itemCount: photosList.length,
+                      itemCount: data.length,
                       itemBuilder: (context, index) {
-                        return ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage:
-                                NetworkImage(photosList[index].url.toString()),
+                        return Card(
+                          child: Column(
+                            children: [
+                              ReusbaleRow(
+                                title: 'name',
+                                value: data[index]['name'].toString(),
+                              ),
+                              ReusbaleRow(
+                                title: 'Username',
+                                value: data[index]['username'].toString(),
+                              ),
+                              ReusbaleRow(
+                                title: 'address',
+                                value:
+                                    data[index]['address']['street'].toString(),
+                              ),
+                              ReusbaleRow(
+                                title: 'Lat',
+                                value: data[index]['address']['geo']['lat']
+                                    .toString(),
+                              ),
+                              ReusbaleRow(
+                                title: 'Lat',
+                                value: data[index]['address']['geo']['lng']
+                                    .toString(),
+                              ),
+                            ],
                           ),
-                          subtitle:
-                              Text(snapshot.data![index].title.toString()),
-                          title: Text('Notes id:' +
-                              snapshot.data![index].id.toString()),
                         );
                       });
-                }),
-          ),
+                }
+              },
+            ),
+          )
         ],
       ),
     );
   }
 }
 
-class Photos {
-  String title, url;
-  int id;
-  Photos({required this.title, required this.url, required this.id});
+class ReusbaleRow extends StatelessWidget {
+  String title, value;
+  ReusbaleRow({Key? key, required this.title, required this.value})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title),
+          Text(value),
+        ],
+      ),
+    );
+  }
 }
