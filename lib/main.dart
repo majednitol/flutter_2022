2d7266e764e6a86fc6f49ac71ddf5953b5d0eecd
Your Name <youremail@yourdomain.com>
2022-03-29 04:02:12 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MaterialApp(
-//     title: "Flutter",
-//     home: HomePage(),
-//     theme: ThemeData(primarySwatch: Colors.blue),
-//   ));
-// }
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({Key? key}) : super(key: key);
-
-//   @override
-//   _HomePageState createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   int value = 0;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text('setState and Provider'),
-//         ),
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Container(
-//                   child: Text(
-//                 value.toString(),
-//                 style: TextStyle(fontSize: 30),
-//               )),
-//               SizedBox(
-//                 height: 40,
-//               ),
-//               ElevatedButton(
-//                   style: ElevatedButton.styleFrom(
-//                     primary: Colors.red,
-//                   ),
-//                   onPressed: () {
-//                     setState(() {
-//                       value++;
-//                     });
-//                   },
-//                   child: Text("click here"))
-//             ],
-//           ),
-//         ));
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 import 'package:flutter_2022/data.dart';
@@ -108,10 +55,10 @@ class _HomePageState extends State<HomePage> {
                     primary: Colors.red,
                   ),
                   onPressed: () {
-                  Navigator.push(
-                   context,
-                   MaterialPageRoute(builder: (context) => Recived()),
-                   );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Recived()),
+                    );
                   },
                   child: Text("sent"))
             ],
